d4d19d45c55e1721d8e0f54990708e854cf12610
Nikolai Klimov <nik.klimov09@yandex.ru>
2026-02-22 15:16:08 +0500

    https://up.htmlacademy.ru/univer-nodejs-api/6/tasks/5

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -5,7 +5,7 @@ import { IConfig, RestConfig, RestSchema } from './shared/libs/config/index.js';
 import { ILogger, PinoLogger } from './shared/libs/logger/index.js';
 import { Component } from './shared/types/index.js';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const container: Container = new Container();
   container.bind<RestApplication>(Component.RestApplication).to(RestApplication);
   container.bind<ILogger>(Component.Logger).to(PinoLogger).inSingletonScope();
