a1edb423de2fb7e97f1a8bbb9769bba39450821e
Nikolai Klimov <nik.klimov09@yandex.ru>
2026-02-22 11:59:52 +0500

    https://up.htmlacademy.ru/univer-nodejs-api/6/tasks/5

---
+++ src/shared/libs/logger/index.ts
@@ -0,0 +1,1 @@
+export * from './interfaces/logger.interface.js';
---
+++ src/shared/libs/logger/interfaces/logger.interface.ts
@@ -0,0 +1,6 @@
+export interface ILogger {
+  info(message: string, ...args: unknown[]): void;
+  warn(message: string, ...args: unknown[]): void;
+  error(message: string, error: Error, ...args: unknown[]): void;
+  debug(message: string, ...args: unknown[]): void;
+}
